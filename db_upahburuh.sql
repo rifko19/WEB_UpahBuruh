f2fe002a6a89b6c15659670ed002659b078bd61e
Rifko Akbar <120491879+rifko19@users.noreply.github.com>
2023-12-03 17:53:26 +0700

    db_upahburuh.sql

--- db_upahburuh.sql
+++ db_upahburuh.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 28 Nov 2023 pada 15.11
+-- Waktu pembuatan: 03 Des 2023 pada 11.46
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.0.30
 
@@ -66,6 +66,16 @@ CREATE TRIGGER `after_delete_tb_buruh` AFTER DELETE ON `tb_buruh` FOR EACH ROW B
 END
 $$
 DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `generate_id_buruh` BEFORE INSERT ON `tb_buruh` FOR EACH ROW BEGIN
+    -- Tentukan panjang ID yang diinginkan
+    SET @id_length = 6;
+    
+    -- Bangkitkan nilai acak untuk ID
+    SET NEW.id_buruh = LPAD(FLOOR(RAND() * POW(10, @id_length)), @id_length, '0');
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -89,7 +99,9 @@ INSERT INTO `tb_gaji` (`id_Gaji`, `GajiBuruhLepas`, `GajiOperator`, `id_buruh`) 
 ('035369', 135000, NULL, '002093'),
 ('037089', 165000, NULL, '025540'),
 ('037439', NULL, 330000, '008753'),
-('039151', NULL, 360000, '001006');
+('039151', NULL, 360000, '001006'),
+('G2990', 0, NULL, '025540'),
+('G8284', -15000, NULL, '025540');
 
 -- --------------------------------------------------------
 
@@ -111,7 +123,9 @@ CREATE TABLE `tb_jam_kerja` (
 --
 
 INSERT INTO `tb_jam_kerja` (`id_JamKerja`, `Jam_Mulai`, `Jam_Selesai`, `Tanggal`, `TotalJamKerja`, `id_buruh`) VALUES
+('002202', '05:14:00', '05:14:00', '2023-12-21', 0, '025540'),
 ('004643', '06:15:00', '19:30:00', '2023-11-12', 13, '020139'),
+('004964', '07:00:00', '05:17:00', '2023-12-06', -1, '025540'),
 ('005095', '07:30:00', '19:00:00', '2023-11-27', 11, '025540'),
 ('006301', '07:00:00', '18:00:00', '2023-11-10', 11, '008753'),
 ('007784', '07:00:00', '16:00:00', '2023-11-10', 9, '002093'),
@@ -155,7 +169,7 @@ CREATE TRIGGER `tb_JamKerja_BEFORE_INSERT` BEFORE INSERT ON `tb_jam_kerja` FOR E
     -- Insert atau update ke tb_Gaji
     INSERT INTO tb_Gaji (id_Gaji, GajiBuruhLepas, GajiOperator, id_buruh)
     VALUES
-        (CONCAT('03', LPAD(FLOOR(RAND() * 10000), 4, '0')), CASE WHEN (SELECT Kategori_Keahlian FROM tb_Buruh WHERE id_buruh = NEW.id_buruh) = 'Buruh Kasar' THEN NEW.TotalJamKerja * 15000 ELSE NULL END,
+        (CONCAT('G', LPAD(FLOOR(RAND() * 10000), 4, '0')), CASE WHEN (SELECT Kategori_Keahlian FROM tb_Buruh WHERE id_buruh = NEW.id_buruh) = 'Buruh Kasar' THEN NEW.TotalJamKerja * 15000 ELSE NULL END,
          CASE WHEN (SELECT Kategori_Keahlian FROM tb_Buruh WHERE id_buruh = NEW.id_buruh) = 'Buruh Operator' THEN NEW.TotalJamKerja * 30000 ELSE NULL END,
          NEW.id_buruh);
 
@@ -185,10 +199,15 @@ CREATE TABLE `tb_laporan` (
 
 INSERT INTO `tb_laporan` (`id_Laporan`, `Kategori_Keahlian`, `id_buruh`, `id_JamKerja`, `id_Gaji`) VALUES
 ('L0164', 'Buruh Kasar', '025540', '005095', '037089'),
+('L0293', 'Buruh Kasar', '025540', '004964', 'G8284'),
 ('L0343', 'Buruh Operator', '001006', '008294', '039151'),
+('L0661', 'Buruh Kasar', '025540', '004964', '037089'),
 ('L1211', 'Buruh Operator', '008753', '006301', '037439'),
+('L1263', 'Buruh Kasar', '025540', '002202', '037089'),
 ('L5168', 'Buruh Operator', '020139', '004643', '031820'),
-('L8448', 'Buruh Kasar', '002093', '007784', '035369');
+('L7345', 'Buruh Kasar', '025540', '002202', 'G2990'),
+('L8448', 'Buruh Kasar', '002093', '007784', '035369'),
+('L8455', 'Buruh Kasar', '025540', '004964', 'G2990');
 
 -- --------------------------------------------------------
 
@@ -210,11 +229,36 @@ CREATE TABLE `view_laporan` (
 -- --------------------------------------------------------
 
 --
+-- Stand-in struktur untuk tampilan `view_laporan3`
+-- (Lihat di bawah untuk tampilan aktual)
+--
+CREATE TABLE `view_laporan3` (
+`id_Laporan` varchar(20)
+,`Nama_Buruh` varchar(100)
+,`Kategori_Keahlian` varchar(100)
+,`TotalJamKerja` int(11)
+,`Tanggal_Kerja` date
+,`GajiBuruhLepas` int(11)
+,`GajiOperator` int(11)
+);
+
+-- --------------------------------------------------------
+
+--
 -- Struktur untuk view `view_laporan`
 --
 DROP TABLE IF EXISTS `view_laporan`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_laporan`  AS SELECT `tb_laporan`.`id_Laporan` AS `id_Laporan`, `tb_laporan`.`Kategori_Keahlian` AS `Kategori_Keahlian`, `tb_buruh`.`Nama` AS `Nama_Buruh`, `tb_jam_kerja`.`Jam_Mulai` AS `Jam_Mulai`, `tb_jam_kerja`.`Jam_Selesai` AS `Jam_Selesai`, `tb_jam_kerja`.`Tanggal` AS `Tanggal_JamKerja`, `tb_gaji`.`GajiBuruhLepas` AS `GajiBuruhLepas`, `tb_gaji`.`GajiOperator` AS `GajiOperator` FROM (((`tb_laporan` left join `tb_buruh` on(`tb_laporan`.`id_buruh` = `tb_buruh`.`id_buruh`)) left join `tb_jam_kerja` on(`tb_laporan`.`id_JamKerja` = `tb_jam_kerja`.`id_JamKerja`)) left join `tb_gaji` on(`tb_laporan`.`id_Gaji` = `tb_gaji`.`id_Gaji`)) ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur untuk view `view_laporan3`
+--
+DROP TABLE IF EXISTS `view_laporan3`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_laporan3`  AS SELECT `l`.`id_Laporan` AS `id_Laporan`, `b`.`Nama` AS `Nama_Buruh`, `b`.`Kategori_Keahlian` AS `Kategori_Keahlian`, `jk`.`TotalJamKerja` AS `TotalJamKerja`, `jk`.`Tanggal` AS `Tanggal_Kerja`, `g`.`GajiBuruhLepas` AS `GajiBuruhLepas`, `g`.`GajiOperator` AS `GajiOperator` FROM (((`tb_laporan` `l` join `tb_buruh` `b` on(`l`.`id_buruh` = `b`.`id_buruh`)) join `tb_jam_kerja` `jk` on(`l`.`id_JamKerja` = `jk`.`id_JamKerja`)) join `tb_gaji` `g` on(`l`.`id_Gaji` = `g`.`id_Gaji`)) ;
 
 --
 -- Indexes for dumped tables
